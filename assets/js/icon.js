ca34083e9096538712af1e3155f35f2e33dc0019
Thanh <mama_strongman308@slmail.me>
2026-03-19 11:59:13 +0700

    Changed icon

--- assets/js/icon.js
+++ assets/js/icon.js
@@ -1,5 +1,5 @@
 const link = document.createElement("link");
 link.rel = "icon";
 link.type = "image/x-icon";
-link.href = "https://lh7-rt.googleusercontent.com/docsz/AD_4nXf2J8ImHMcg8zX8pCrif15u5M5BlKv62u59C6QvRSd77-M2xo00fEiW4dcx1nZR4k5Qrk6m3M4XZvU3OqC-wgisTdJPr_B-6icNEZ-hOYWJYTPMu7ONlVfzjHnoztevy86K0rY-?key=uM4T9K2cZ_zv-lmxAN8BfA";
+link.href = "https://lh7-rt.googleusercontent.com/docsz/AD_4nXdv952Om6aN_54i_rSn3yLD8bFLryLQ3a83qBGhmc0v3SeFedZthwqESlCh1qSf80M3L2kWUi05VOqoODDk3eI7Kd_HqjsIW7dwuc-BipB_7H8hP8eQfu1J2M4pmy0sMFohmsjd4w?key=uM4T9K2cZ_zv-lmxAN8BfA";
 document.head.appendChild(link);
